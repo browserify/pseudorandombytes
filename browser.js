4757683ffc9835c0ff01099f73517b8d96075674
Calvin Metcalf <calvin.metcalf@gmail.com>
2015-10-04 16:56:03 -0400

    try to use randombytes first

--- browser.js
+++ browser.js
@@ -1,10 +1,18 @@
+
+
 var PRNG = require('./prng');
+var randombytes = require('randombytes');
+try {
+  randombytes(8);
+  module.exports = randombytes;
+} catch (e) {
+  var prng = new PRNG(function () {
+    return (Math.random().toString() + Date.now().toString());
+  });
 
-var prng = new PRNG(function () {
-  return (Math.random().toString() + Date.now().toString());
-});
+  module.exports = psudoRandomBytes;
+}
 
-module.exports = psudoRandomBytes;
 function psudoRandomBytes(len) {
   return prng.getBytes(len);
 }
